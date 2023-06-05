865dd1ba9c60088394e5058c5834b413f828810f
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-06-05 16:22:19 +0200

    started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
+    colors: {
+      "main-green": "#51cf66",
+      "dark-gray": "#343a40",
+      "light-gray": "#f8f9fa",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
