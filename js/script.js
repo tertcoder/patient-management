4011aeb74ed04cd87f6875e49b00656d2ca30c54
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-06-08 08:38:33 +0200

    profile style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const linksContainer = document.querySelector(".navigation__links");
 const linksSection = document.querySelectorAll(".tab-content");
+const openProfile = document.querySelector(".btnOpen");
+const profilesContainer = document.querySelector(".studentProfiles");
+const studentProfile = document.querySelector(".studentProfile");
 
 linksContainer.addEventListener("click", function (e) {
   const link_clicked = e.target.closest(".nav__link");
@@ -9,4 +12,10 @@ linksContainer.addEventListener("click", function (e) {
   document
     .querySelector(`.section__${link_clicked.dataset.tab}`)
     .classList.add("tab-content--active");
+});
+
+openProfile.addEventListener("click", function (e) {
+  e.preventDefault();
+  profilesContainer.classList.add("hidden");
+  studentProfile.classList.remove("hidden");
 });
