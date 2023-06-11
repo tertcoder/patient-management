cfc78e2daf17862cb0b1b81948dbb637d850e2e7
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-06-11 13:24:31 +0200

    notification tab

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const studentProfile = document.querySelector(".studentProfile");
 linksContainer.addEventListener("click", function (e) {
   const link_clicked = e.target.closest(".nav__link");
 
+  if (!link_clicked) return;
+
   linksSection.forEach((tab) => tab.classList.remove("tab-content--active"));
 
   document
@@ -18,4 +20,22 @@ openProfile.addEventListener("click", function (e) {
   e.preventDefault();
   profilesContainer.classList.add("hidden");
   studentProfile.classList.remove("hidden");
+});
+
+const form = document.querySelector("[data-form]");
+const newModal = document.querySelector("[data-new]");
+const noticeBtn = document.querySelector("[data-notice-btn]");
+form.onsubmit = () => {
+  localStorage.setItem("newNotice", "1");
+};
+
+if (localStorage.getItem("newNotice") == "1") {
+  newModal.classList.remove("hidden");
+} else if (localStorage.getItem("newNotice") != "1") {
+  newModal.classList.add("hidden");
+}
+
+noticeBtn.addEventListener("click", () => {
+  localStorage.setItem("newNotice", false);
+  newModal.classList.add("hidden");
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeOut: "fadeOut 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        fadeOut: {
+          "0%, 100%": { opacity: "1", scale: "1" },
+          "50%": { opacity: ".5", scale: "1.5" },
+        },
+      },
+    },
   },
   plugins: [],
 };
